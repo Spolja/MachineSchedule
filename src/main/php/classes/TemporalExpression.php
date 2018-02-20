c969db6d833b31f08623615a62291a05c5a0bec5
Luka Špoljarić <Spolja@users.noreply.github.com>
2018-02-20 17:28:36 +0100

    classes added to project structure

---
+++ src/main/php/classes/TemporalExpression.php
@@ -0,0 +1,34 @@
+<?php
+class TemporalExpression {
+    private $startTime;
+    private $endTime;
+    
+    public function __construct($startHour, $endTime) {
+        $this->endTime = DateTime::createFromFormat('d.m.Y. H:i', $endTime)->getTimestamp();
+        $this->startTime = $this->calculateStartTime($startHour);
+   }
+   
+   private function calculateStartTime($startHour){
+       $endTimeHour = DateTime::createFromFormat('U', $this->endTime)->format('H:i');
+       $endTimeDate = DateTime::createFromFormat('U', $this->endTime)->format('d.m.Y.');
+       
+       $startTimeHour = $startHour;
+       $startTimeDate = new DateTime($endTimeDate);
+       
+       //TODO: check strtotime format expectation of the hour string, atm it is "18:30", seconds not included
+       if(strtotime($endTimeHour) <= strtotime($startTimeHour)){
+           $startTimeDate->modify("-1 day");
+       }
+       
+       $startTime = $startTimeDate . " " . $startTimeHour;
+       $startTime = DateTime::createFromFormat('d.m.Y. H:i', $startTime)->getTimestamp();
+       
+       return $startTime;
+   }
+   
+    public function __get($property) {
+        if (property_exists($this, $property)) {
+          return $this->$property;
+        }
+    }
+}
